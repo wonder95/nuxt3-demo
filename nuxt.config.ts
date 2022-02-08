e89059fecbc1e7116ab9631658ba53b6c9ff0622
wonder95 <steve@smgaweb.com>
2022-02-07 16:52:25 -0800

    Use vite config for Coinlore URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    // vite: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'https://api.coinlore.net/api',
-    //             changeOrigin: true,
-    //             rewrite: (path) => path.replace(/^\/api/, ''),
-    //             secure: false
-    //         },
-    //     }
-    // }
+    vite: {
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'https://api.coinlore.net/api',
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/api/, ''),
+                    secure: false,
+                },
+            },
+        },
+    },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,13 @@
       <tr>
         <th>Name</th>
         <th>Symbol</th>
-        <th>Slug</th>
-        <th>Price</th>
-        <th>Details</th>
+        <th>Rank</th>
+        <th>ID</th>
       </tr>
-      <tr v-for="currency in data" :key="id">
+      <tr v-for="currency in data.data" :key="data.id">
         <td>{{ currency.name }}</td>
         <td>{{ currency.symbol }}</td>
-        <td>{{ currency.slug }}</td>
-        <td>{{ currency.quote.USD.price }}</td>
+        <td>{{ currency.rank }}</td>
         <td>
           <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
         </td>
@@ -25,58 +23,6 @@
   </main>
 </template>
 
-<script>
-import {ref} from 'vue';
-import { onMounted } from "vue";
-export default {
-  name: 'CurrencyList',
-  props: {},
-  setup() {
-    const data = ref(null);
-    const loading = ref(true);
-    const error = ref(null);
-
-    function fetchData() {
-        return fetch('api/tickers', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((res) => {
-              return res.json()
-            })
-            .then((json) => {
-                data.value = json.data;
-            })
-            .catch(err => {
-              error.value = err;
-              // In case a custom JSON error response was provided
-              if (err.json) {
-                return err.json.then(json => {
-                  // set the JSON response message
-                  error.value.message = json.message;
-                });
-              }
-            })
-            .then(() => {
-              loading.value = false;
-            });
-    }
-
-    onMounted(() => {
-      fetchData();
-    })
-
-    return {
-      data,
-      loading,
-      error
-    };
-  },
-}
+<script setup lang="ts">
+const { data } = await useFetch('http://localhost:3000/api/tickers/')
 </script>
-
-<style scoped>
-
-</style>
